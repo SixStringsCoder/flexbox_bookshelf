b2eb5eb5b7e8e0cc16a5cb46478c7781b8f99fda
SixStringsCoder <rshanlon@gmail.com>
2017-08-25 23:48:30 -0700

    switch to CSS-only solution using flex box, jQuery Modal window not used

--- static/js/script.js
+++ static/js/script.js
@@ -4,14 +4,20 @@
 
 
 
-// Shows Modal when touching the book or book title
-$('.myBtn').on('click', function() {
-    $('.myModal').fadeIn('slow');
-    console.log("Show modal");
-});
 
-// Hides Modal when X is clicked
-$('.close').on('click', function() {
-    $('.myModal').fadeOut('slow');
-    console.log('Hide modal');
-});
+
+
+
+
+
+// // Shows Modal when touching the book or book title
+// $('.myBtn').on('click', function() {
+//     $('.myModal').fadeIn('slow');
+//     console.log("Show modal");
+// });
+//
+// // Hides Modal when X is clicked
+// $('.close').on('click', function() {
+//     $('.myModal').fadeOut('slow');
+//     console.log('Hide modal');
+// });
